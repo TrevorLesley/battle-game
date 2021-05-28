8526d96ed81f07105749de11f7291382aec566e6
Trevor Lesley <tlesley96@gmail.com>
2021-05-28 12:01:29 -0400

    Added battle and player constructors.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,31 @@
-const Battle = new Battle {
+(function() {
+    'use strict';
 
-}
+    // const water = document.getElementById('elements').value = 'water';
+    // const earth = document.getElementById('elements').value = 'earth';
+    // const fire = document.getElementById('elements').value = 'fire';
+    // const air = document.getElementById('elements').value = 'air';
 
-//
-// console.log('hi');
+    const element = document.querySelector('elements');
+
+    console.log(element);
+
+    const Battle = function () {
+        const player = prompt(`Enter player name, then choose your class.`);
+        this.player = new Player;
+        this.comp = new Comp;
+    }
+
+    const Player = function ({ name } = {}) {
+        this.name = name;
+        this.element = 
+        this.health = 100;
+    }
+
+    const Comp = function () {
+        this.health = 100;
+    }
+
+
+
+})();
