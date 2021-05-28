c6e412c3d9cfe03420d9a8535ff392e873febeb6
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-05-28 14:32:04 -0400

    wrote cons functions

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@
 
     const $element = document.querySelector('#elements');
 
-    
+
 
     const Battle = function () {
         const player = prompt(`Enter player name, then choose your class.`);
@@ -18,7 +18,7 @@
 
     const Player = function ({ name } = {}) {
         this.name = name;
-        this.element = 
+        this.element =
         this.health = 100;
     }
 
@@ -28,4 +28,48 @@
 
 
 
+
+
+var health;
+
+function Player (name) {
+  this.name = name;
+  this.health = 100;
+}
+
+function Comp () {
+  this.health = 100;
+}
+
+function Battle () {
+  this.player = new Player();
+  this.enemy = new Comp();
+  this.attack();
+}
+
+  // while(this.player.health > 0 && this.player.health > 0) {
+  // this.attack();
+  //
+  // }
+
+Battle.prototype.attack = function() {
+const random = Math.floor(Math.random() * 10);
+hp = this.player.health - random;
+// return hp;
+console.log(hp);
+}
+
+// Battle.prototype.counter = function() {
+//
+// }
+
+const battle = new Battle;
+
+
+
+
+
+
+//
+// console.log('hi');
 })();
