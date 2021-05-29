d9288c0cea7e07c104b0e1d90c1ae5bc28a9ba01
Trevor Lesley <tlesley96@gmail.com>
2021-05-29 12:01:10 -0400

    Updated image paths.

--- scripts/char-types.js
+++ scripts/char-types.js
@@ -1,29 +1,24 @@
 const charType = ({
     'choiceElement': 'Water',
     'title': 'Water',
-    'Images': [{
+    'Image': '/Users/newuser/Documents/Code/Homework/battle-game/images/female_water_fighter__elemental__by_zachdb_d5qwm87-fullview.jpeg',
 
-    }]
 },
 {
     'choiceElement': 'Earth',
     'title': 'Earth',
-    'Images': [{
+    'Image': '/Users/newuser/Documents/Code/Homework/battle-game/images/earth_elemental_by_stevegoad_d2pjakv.jpeg',
 
-    }]
 },
 {
     'choiceElement': 'Fire',
     'title': 'Fire',
-    'Images': [{
+    'Image': '/Users/newuser/Documents/Code/Homework/battle-game/images/fire_kirin_by_sakimichan_d4r884s.jpeg',
 
-    }]
 },
 {
     'choiceElement': 'Air',
     'title': 'Air',
-    'Images': [{
-
-    }]
+    'Image': '/Users/newuser/Documents/Code/Homework/battle-game/images/wind_knight_by_jasontn_da7v75t.jpeg',
 }
 )
