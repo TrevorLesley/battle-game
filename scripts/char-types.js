07b85f6968b6a0dfa7e71cffb31b1f9c2be578e8
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-05-30 15:07:57 -0400

    added drop down menu to game

--- scripts/char-types.js
+++ scripts/char-types.js
@@ -2,27 +2,27 @@
 const charType = ({
     'results': [{
         'choiceElement': 'Water',
-        'title': 'Water',
-        'Image': '/Users/newuser/Documents/Code/Homework/battle-game/images/female_water_fighter__elemental__by_zachdb_d5qwm87-fullview.jpeg',
+        'Desciption': 'Befrese: Hyrdokensis',
+        'Image': './images/female_water_fighter__elemental__by_zachdb_d5qwm87-fullview.jpeg',
 
     },
     {
         'choiceElement': 'Earth',
-        'title': 'Earth',
-        'Image': '/Users/newuser/Documents/Code/Homework/battle-game/images/earth_elemental_by_stevegoad_d2pjakv.jpeg',
+        'title': 'Gutrium: Geokinesis',
+        'Image': './images/earth_elemental_by_stevegoad_d2pjakv.jpeg',
 
     },
     {
         'choiceElement': 'Fire',
-        'title': 'Fire',
-        'Image': '/Users/newuser/Documents/Code/Homework/battle-game/images/fire_kirin_by_sakimichan_d4r884s.jpeg',
+        'title': 'Vustrite: Pyrokinesis',
+        'Image': './images/fire_kirin_by_sakimichan_d4r884s.jpeg',
 
 
     },
     {
         'choiceElement': 'Air',
-        'title': 'Air',
-        'Image': '/Users/newuser/Documents/Code/Homework/battle-game/images/wind_knight_by_jasontn_da7v75t.jpeg',
+        'title': 'Uthine the Wind Knight: Wind Manipulation',
+        'Image': './images/wind_knight_by_jasontn_da7v75t.jpeg',
     },
-    }]
+    ]
 })
